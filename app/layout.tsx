6e7ce0e3e9f2b831b2fcb94523459afd419b5e0d
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-07-17 16:16:50 +0530

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,20 @@ export default function RootLayout({
           }}
         />
         {/* End Meta Pixel Code */}
+        
+        {/* Clarity Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xnterqf6v7");
+            `
+          }}
+        />
+        {/* End Clarity Tracking Code */}
       </head>
       <body
         className={`${manrope.variable} ${playfair.variable} ${outfit.variable} font-body selection:bg-[#624452] selection:text-[#dbb3c4]`}
